f70f348a5206e4896e308510811cd0ab327753d1
Iliescu Gabriel <83876428+Iliescu-Gabriel@users.noreply.github.com>
2022-03-26 12:28:33 +0200

    Re-upload

--- 5_Minesweeper/modules/getElement.js
+++
@@ -1,7 +0,0 @@
-function getElement(element) {
-  const el = document.querySelector(element);
-  if (el) return el;
-  throw new Error(`Element with selector ${element} not found`);
-}
-
-export default getElement;
